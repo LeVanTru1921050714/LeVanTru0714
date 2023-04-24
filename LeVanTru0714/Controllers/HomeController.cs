566fd5ef05e86c0eb36f9c6481fbe38b4924b5ac
LeVanTru1921050714 <lee27052001@gmail.com>
2023-04-24 15:20:38 +0700

    LeVanTru0714

---
+++ LeVanTru0714/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using LeVanTru0714.Models;
+
+namespace LeVanTru0714.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ LeVanTru0714/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LeVanTru0714.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
